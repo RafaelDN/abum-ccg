2f05907fafcee475182efb082caeb12986041b52
RafaelDN <dal.negro@hotmail.com>
2026-06-03 17:26:52 -0300

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true'
+const isUserOrOrgPage = repositoryName?.endsWith('.github.io')
+
 export default defineConfig({
+  base: isGitHubPagesBuild && repositoryName && !isUserOrOrgPage ? `/${repositoryName}/` : '/',
   plugins: [vue()],
 })
